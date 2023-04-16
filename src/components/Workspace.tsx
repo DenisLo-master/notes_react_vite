0749b1d71e79a5ef046c173fb6227124ec761e79
DENIS LOGINOV <denis.lkg@gmail.com>
2023-04-16 17:42:45 +0600

    firebase sync without user uid

--- src/components/Workspace.tsx
+++ src/components/Workspace.tsx
@@ -3,13 +3,32 @@ import Header from './Header'
 import ListItem from './ListItem'
 import MainArea from './MainArea'
 import { useLayoutContext } from '../hooks/useLayoutContext'
+import { getNotesFromFirebase, setNotesToFirebase } from '../store/action/firebaseExchange'
 
 const Layout = () => {
   const { visible } = useLayoutContext()
 
+
+  const notes = [
+    {
+      title: "first note",
+      body: "first note body",
+    },
+    {
+      title: "first note",
+      body: "first note body",
+    }
+  ]
+
   return (
     <Container size="xl">
       <div className="main">
+        <button onClick={() => {
+          setNotesToFirebase({ user: "denis.lkg@gmail.com", notes })
+        }}>toFB</button>
+        <button onClick={() => {
+          getNotesFromFirebase("denis.lkg@gmail.com")
+        }}>fromFB</button>
         <Header />
         <Box
           className="containerShadow"
