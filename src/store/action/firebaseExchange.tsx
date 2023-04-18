588581d695f4cf8bfc701476a8500c2dd445b6bf
DENIS LOGINOV <denis.lkg@gmail.com>
2023-04-18 16:00:03 +0600

    used mantive markdown editor

--- src/store/action/firebaseExchange.tsx
+++ src/store/action/firebaseExchange.tsx
@@ -1,47 +1,48 @@
 import { ref, getDatabase, child, get, push, set } from 'firebase/database'
-import { firebaseApp } from '../firebase.config'
+import { auth, firebaseApp } from '../firebase.config'
 import { Note } from '../../interfaces/NoteProps'
 
 const db = getDatabase(firebaseApp)
 
 interface UserNotes {
-  user: string
-  notes: Note[]
+    user: string
+    notes: Note[]
 }
 interface Updates {
-  [key: string]: Note[]
+    [key: string]: Note[]
 }
 
 export async function setNotesToFirebase({ user, notes }: UserNotes) {
-  try {
-    //console.log('setNotesToFirebase--', user)
-    //временно пока нет uid будет подставляться вместо userFB
-    const uid = user.replace(new RegExp('\\.', 'g'), '_')
-    const newHashKey = push(child(ref(db), `/notes_data/${uid}/notes`)).key
-    if (!newHashKey) return
-    const updates: Updates = {}
-    updates[newHashKey] = notes
-    console.log(updates)
-    //await set(ref(db, `/notes_data/${uid}/notes/`), updates)
-  } catch (err) {
-    console.error('Error setNotesToFirebase', user, err)
-  }
+    try {
+        //console.log('setNotesToFirebase--', user)
+        //временно пока нет uid будет подставляться вместо userFB
+
+        const uid = user.replace(new RegExp('\\.', 'g'), '_')
+        const newHashKey = push(child(ref(db), `/notes_data/${uid}/notes`)).key
+        if (!newHashKey) return
+        const updates: Updates = {}
+        updates[newHashKey] = notes
+        console.log(updates)
+        //await set(ref(db, `/notes_data/${uid}/notes/`), updates)
+    } catch (err) {
+        console.error('Error setNotesToFirebase', user, err)
+    }
 }
 
 export async function getNotesFromFirebase(user: string) {
-  try {
-    //console.log('getNotesFromFirebase--', user)
-    const uid = user.replace(new RegExp('\\.', 'g'), '_')
-    //console.log(uid)
-    const snapshot = await get(child(ref(db), `/notes_data/${uid}/notes/`))
+    try {
+        //console.log('getNotesFromFirebase--', user)
+        const uid = user.replace(new RegExp('\\.', 'g'), '_')
+        //console.log(uid)
+        const snapshot = await get(child(ref(db), `/notes_data/${uid}/notes/`))
 
-    if (snapshot.exists()) {
-      const hash = Object.keys(snapshot.val())[0]
-      const notes = snapshot.val()[hash]
-      console.log('Notes', notes)
-      return notes
+        if (snapshot.exists()) {
+            const hash = Object.keys(snapshot.val())[0]
+            const notes = snapshot.val()[hash]
+            console.log('Notes', notes)
+            return notes
+        }
+    } catch (err) {
+        console.error('Error getNotesFromFirebase', user, err)
     }
-  } catch (err) {
-    console.error('Error getNotesFromFirebase', user, err)
-  }
 }
