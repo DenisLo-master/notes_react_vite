ef72c6e4eb436bd405ef97be139ac3e298f9f305
DENIS LOGINOV <denis.lkg@gmail.com>
2023-04-19 22:34:25 +0600

    image store in progress

--- src/store/action/UpdateLocalDB.ts
+++ src/store/action/UpdateLocalDB.ts
@@ -1,7 +1,7 @@
 import { db } from './NotesDB'
 import { UpdateNote } from '../../interfaces/NoteProps'
 
-export async function updateNotes(note: UpdateNote): Promise<void> {
+export async function updateNotes(note: Note): Promise<void> {
   try {
     console.log('Updating notes-------', note)
     await db.updateNote(note)
