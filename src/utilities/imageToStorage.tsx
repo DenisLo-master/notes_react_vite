d1158ad745646020a7cb4e77a70ea11e0ca363f8
AntonKulagin <i-gorusich@mail.ru>
2023-04-22 22:10:13 +0300

    fix after rebase

--- src/utilities/imageToStorage.tsx
+++ src/utilities/imageToStorage.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Note } from '../interfaces/NoteProps'
-import { uploadFiles } from '../store/action/fbStorageExchange.ts';
-import { deleteImageDB, getImageDB } from '../store/action/imageDB';
-import { updateNoteDB } from '../store/action/noteDB';
+import { uploadFiles } from '../store/action/fbStorageExchange.ts'
+import { deleteImageDB, getImageDB } from '../store/action/imageDB'
+import { updateNoteDB } from '../store/action/noteDB'
 
 interface ImageToStorageProps {
   uid: string
@@ -10,46 +10,45 @@ interface ImageToStorageProps {
 }
 
 export async function imageToStorage({ uid, note }: ImageToStorageProps): Promise<boolean> {
-    return new Promise(async (resolve) => {
-        const newBody = document.createElement("div");
-        newBody.innerHTML = note.body
-        const imageArray = [...newBody.getElementsByTagName("img")]
-        for (let i = 0; i < imageArray.length; i++) {
-            const image = imageArray[i];
-            if (image) {
-                if (image.alt === "local" && image.title) {
-                    const fileData = await getImageDB(
-                        {
-                            noteId: note.id,
-                            fileName: image.title
-                        })
-                    const fileBlob = fileData && dataURLtoBlob(fileData)
-                    const link = fileBlob && await uploadFiles(
-                        {
-                            uid,
-                            noteId: note.id,
-                            fileName: image.title,
-                            file: fileBlob
-                        })
-                    if (link) {
-                        image.src = link
-                        image.alt = "image"
-                        await deleteImageDB({ noteId: note.id, fileName: image.title })
-                    } else {
-                        const imageUrl = fileBlob && URL.createObjectURL(fileBlob)
-                        if (imageUrl) {
-                            image.src = imageUrl
-                            resolve(false)
-                        }
-                    }
-                }
+  return new Promise(async (resolve) => {
+    const newBody = document.createElement('div')
+    newBody.innerHTML = note.body
+    const imageArray = [...newBody.getElementsByTagName('img')]
+    for (let i = 0; i < imageArray.length; i++) {
+      const image = imageArray[i]
+      if (image) {
+        if (image.alt === 'local' && image.title) {
+          const fileData = await getImageDB({
+            noteId: note.id,
+            fileName: image.title,
+          })
+          const fileBlob = fileData && dataURLtoBlob(fileData)
+          const link =
+            fileBlob &&
+            (await uploadFiles({
+              uid,
+              noteId: note.id,
+              fileName: image.title,
+              file: fileBlob,
+            }))
+          if (link) {
+            image.src = link
+            image.alt = 'image'
+            await deleteImageDB({ noteId: note.id, fileName: image.title })
+          } else {
+            const imageUrl = fileBlob && URL.createObjectURL(fileBlob)
+            if (imageUrl) {
+              image.src = imageUrl
+              resolve(false)
             }
           }
         }
-        note.body = newBody.innerHTML
-        await updateNoteDB({ ...note, sync: false })
-        resolve(true)
-    })
+      }
+    }
+    note.body = newBody.innerHTML
+    await updateNoteDB({ ...note, sync: false })
+    resolve(true)
+  })
 }
 
 export function dataURLtoBlob(dataURL: string): Blob | null {
