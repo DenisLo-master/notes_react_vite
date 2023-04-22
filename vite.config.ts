25b24ca445335eaa5c88f70ded67280af0d7e987
DENIS LOGINOV <denis.lkg@gmail.com>
2023-04-23 01:51:12 +0600

    vite config splite chunk

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 500000, // Устанавливаем предупреждение о размере чанка на 500 кБ
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Создаем отдельный чанк для зависимостей из node_modules
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
